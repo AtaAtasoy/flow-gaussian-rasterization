241a237bdc158264318ef42041d6082793b72a4b
AtaAtasoy <h.ataatasoy@gmail.com>
2024-06-19 16:04:23 +0200

    update NUM_CHANNELS for flow

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="flow_gaussian_rasterization",
+    packages=['flow_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="flow_gaussian_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
